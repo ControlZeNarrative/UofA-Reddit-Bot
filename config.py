f28274b8d716d8379c908613e2788986c0e53347
ControlZeNarrative <98216145+ControlZeNarrative@users.noreply.github.com>
2024-02-17 20:46:32 -0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+username = "??"
+password = "??"
+client_id = "??"
+client_secret = "??"
+user_agent = "??" #This can be any string, doesn't actually matter
+
+
+my_account = "??" #Username of account to send message to
+subreddit = "?" #Name of subreddit to observe. DO NOT INCLUDE r/
